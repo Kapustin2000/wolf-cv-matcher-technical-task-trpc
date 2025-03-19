b8bf7f26f8a62e5e33b4533b8fa70f57905271b5
Mykhailo Kapustin <kapustinomm@gmail.com>
2025-03-19 12:36:55 +0100

    Remove Redis queues

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,10 +3,7 @@
  */
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
 import { router } from './trpc.js';
-import { startWorkers } from './lib/queue.js';
 import { matchRouter } from './routers/match.js';
-
-startWorkers(['matchQueue']).catch(console.error);
 
 export const appRouter = router({
   match: matchRouter,
